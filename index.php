6c0b6c46e1d1437133626565ee6f2c68eee19114
Danny Mendl <danny@danmendl.com>
2020-11-29 23:01:16 -0500

    Init commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Measurement Conversion</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+
+    <div id="main-content">
+
+      <h1>Measurement Conversion</h1>
+  
+      <ul id="measurement-types">
+        <li><a href="volume.php">Brewery Cask Conversions</a></li>
+        <li><a href="outdated-irish-length-measurements.php">Outdated Irish Length Measurements</a></li>
+      </ul>
+
+      <a href="../index.html">Return to Main</a>
+  
+    </div>
+  </body>
+</html>
